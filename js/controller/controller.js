8cb25a1b196d4cce6c70b526cee0619a12da9c5d
BrunoTerres <brunoeterres@gmail.com>
2020-01-23 02:16:41 -0300

    Identificando e concatenando números em array

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -38,9 +38,52 @@
 
     }
 
+    getLastOperation(){
+
+        return this._operation[this._operation.length - 1];
+
+    }
+
+    setLastOperation(value){
+
+        this._operation[this._operation.length -1] = value;
+
+    }
+
+    isOperator(value) {
+
+        return (['+', '-', 'x', '÷'].indexOf(value) > -1);
+
+    }
+
+
     addOperation(value){
 
-        this._operation.push(value);
+        console.log('A ', isNaN(this.getLastOperation()));
+
+        if (isNaN(this.getLastOperation())){
+
+            if (this.isOperator(value)) {
+
+                this.setLastOperation(value);
+
+            } else if(isNaN(value)) {
+
+                console.log(value);
+
+            } else {
+
+                this._operation.push(value);
+
+            }
+
+
+        } else {
+            let newValue = this.getLastOperation().toString() + value.toString();
+            this.setLastOperation(parseInt(newValue));
+
+        }
+
 
         console.log(this._operation);
 
@@ -65,19 +108,19 @@
                break;
 
            case 'adc':
-
+               this.addOperation('+');
                break;
 
            case 'sub':
-
+               this.addOperation('-');
                break;
 
            case 'div':
-
+               this.addOperation('÷');
                break;
 
            case 'tms':
-
+               this.addOperation('x');
                break;
 
            case 'eql':
